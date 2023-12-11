95308058a5d84a587e9d1e37bbc2f5efe227f40d
CamilaCerqueira <camilaroberia@gmail.com>
2023-12-11 08:54:20 -0300

    Initial commit

---
+++ PetShopTest/src/test/java/com/btg/PetShopTest/PetShopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.PetShopTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
